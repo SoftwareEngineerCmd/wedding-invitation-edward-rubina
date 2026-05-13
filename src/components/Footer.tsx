218cc2e5a36c5836c4dd3da432be548c0a0f67e0
Tigran <mherannas@gmail.com>
2026-05-13 23:04:29 +0400

    refactor: remove smooth scroll code, update CSS variables, add assets and fix layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ export const Footer = () => {
       <p className="heading-script gold-shimmer text-center">
         Սիրով սպասում ենք Ձեզ
       </p>
-      <p className="hero-date text-center">Edward &amp; Rubina · 03.07.2026</p>
+      <p className="hero-date text-center">
+        Edward &amp; Rubina <br /> 03.07.2026
+      </p>
     </footer>
   );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import Photo from "../assets/wedding/photo-1.1.jpg";
-import MonogramER from "../assets/monogram-er.png";
+import MonogramER from "../assets/monogram-brown.png";
 import { AutoAudio } from "./AutoAudio";
 import mp3 from "../assets/audio/John Legend - All of Me (Official Video).mp3";
 
@@ -15,22 +15,22 @@ export const Hero = () => {
           />
         </div>
         <div className="hero-overlay" />
-        {/* <div className="h-1/1 flex flex-col  justify-between"> */}
 
         <div className="hero-content">
           <img
             src={MonogramER}
             alt="E & R monogram"
-            className="monogram"
+            className="monogram object-cover"
             width={1024}
             height={1024}
           />
           <AutoAudio loop volume={0.8} src={mp3} />
-          <h1 className="names">Edward &amp; Rubina</h1>
+          <h1 className="names">
+            Edward &amp; Rubina <br />
+          </h1>
 
           <p className="tagline">Our forever begins today</p>
         </div>
-        {/* </div> */}
       </header>
     </>
   );
